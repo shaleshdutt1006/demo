5f00d6c7f35ae070e0a68e599671a1475bd83d0b
shalesh1006 <shaleshdutt1006@gmail.com>
2022-08-19 18:53:23 +0530

    adding files

---
+++ src/BinaryConversion.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BinaryConversion {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int[] array;
+		array = new int[1000];
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the decimal value you want to change : ");
+
+		int number = sc.nextInt();
+
+		while (number > 0) {
+			{
+				array[i] = number % 2;
+				number = number / 2;
+				i++;
+
+			}
+
+		}
+		for (int j = i - 1; j >= 0; j--) {
+			System.out.print(array[j]);
+		}
+
+	}
+
+}
